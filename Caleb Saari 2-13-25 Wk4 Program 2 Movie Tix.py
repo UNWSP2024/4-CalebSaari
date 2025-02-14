35a6a72597e4f49614d6618ac435b701819cb2bd
CalebSaari <saaricaleb32@gmail.com>
2025-02-13 20:15:18 -0600

    Update Caleb Saari 2-13-25 Wk4 Program 2 Movie Tix.py
    Added Flow Chart.

--- Caleb Saari 2-13-25 Wk4 Program 2 Movie Tix.py
+++ Caleb Saari 2-13-25 Wk4 Program 2 Movie Tix.py
@@ -1,4 +1,8 @@
-total = 0.0
+# Program #2: Movie Tix
+# Write a program that has the user input various movie names and how many 
+# tickets are desired for each movie.  
+# At the end of the program it prints out the total number of tickets desired by the user.  
+# Use either a "for loop" or "while loop" to accomplish this.total = 0.0
 
 another_movie = True
 
